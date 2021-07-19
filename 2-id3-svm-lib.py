5012f317a2a50f812f87a3bb907843c8a5460915
Yasas <2017is070@stu.ucsc.cmb.ac.lk>
2021-07-19 11:59:24 +0530

    20000 examples

--- 2-id3-svm-lib.py
+++ 2-id3-svm-lib.py
@@ -36,8 +36,8 @@ def load_data(data_file_path):
 
 
 def run_id3_cifar(x_train, y_train, x_test, y_test):
-    x_train = x_train[:1000, :].astype(float)
-    y_train = np.squeeze(y_train[:1000, :])
+    x_train = x_train[:20000, :].astype(float)
+    y_train = np.squeeze(y_train[:20000, :])
     y_test = np.squeeze(y_test)
     x_test = x_test.astype(float)
     x_train = np.reshape(x_train, (x_train.shape[0], -1))
